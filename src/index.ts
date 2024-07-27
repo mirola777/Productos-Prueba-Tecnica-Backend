2926799e4b45d6ae91ee80ae5e5ab35f381d99bd
mirola777 <jmmunoza@eafit.edu.co>
2024-07-27 12:35:18 -0500

    refactor: Update server initialization logic and add AppServer class
    This commit refactors the server initialization logic by introducing a new `AppServer` class. The `index.ts` file is updated to use the `AppServer` class for configuring middlewares, routes, and error handling. The `AppServer` class encapsulates the server setup and provides a cleaner and more modular approach to server initialization. This change improves the maintainability and readability of the codebase.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,57 @@
 import dotenv from "dotenv";
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
+import http from "http";
+import { ErrorMiddleware } from "./middlewares/ErrorMiddleware";
+import { Middlewares } from "./middlewares/Middlewares";
+import { RegisteredRoutes } from "./routes/Routes";
 
-dotenv.config();
+const isProduction = process.env.NODE_ENV === "production";
+const isDevelopment = process.env.NODE_ENV === "development";
 
-const app: Express = express();
-const port = process.env.PORT || 3000;
+export class AppServer {
+  private static app: Express;
+  private server: http.Server;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Seasrver");
-});
+  constructor() {
+    dotenv.config();
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+    AppServer.app = express();
+    this.server = http.createServer(AppServer.app);
+
+    this.configureMiddlewares();
+    this.configureRoutes();
+    this.configureErrorHandling();
+
+    if (isDevelopment || isProduction) {
+      this.listen();
+    }
+  }
+
+  public static getApp() {
+    if (!AppServer.app) new AppServer();
+
+    return AppServer.app;
+  }
+
+  public listen() {
+    const port = process.env.PORT || 3000;
+
+    this.server.listen(port, () => {
+      console.log(`[server]: Server is running at http://localhost:${port}`);
+    });
+  }
+
+  private configureMiddlewares() {
+    AppServer.app.use(Middlewares);
+  }
+
+  private configureRoutes() {
+    AppServer.app.use("/", RegisteredRoutes);
+  }
+
+  private configureErrorHandling() {
+    AppServer.app.use(ErrorMiddleware);
+  }
+}
+
+if (isDevelopment || isProduction) new AppServer();
